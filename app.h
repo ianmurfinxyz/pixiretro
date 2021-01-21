d16f67e5cc2e90ead1ab640b2b329d08d2b5b88b
ianmurfinxyz <ianmurfin@protonmail.com>
2021-01-21 09:00:55 +0000

    refactored app class

--- app.h
+++ app.h
@@ -1,58 +1,77 @@
-class Application;
+#ifndef _APP_H_
+#define _APP_H_
 
-class ApplicationState
+#include <memory>
+
+namespace pxr
+{
+
+class App;
+
+// Virtual base class for app states. Derive from this class to create app 'modes'
+// that can be switched between, e.g. a splash screen, a menu, a gameplay state etc.
+class AppState
 {
 public:
-  ApplicationState(Application* app) : _app(app) {}
-  virtual ~ApplicationState() = default;
-  virtual void initialize(Vector2i worldSize, int32_t worldScale) = 0;
+  AppState(App* owner) : _owner(owner) {}
+
+  virtual ~AppState() = default;
+  virtual bool onInit() = 0;
   virtual void onUpdate(double now, float dt) = 0;
   virtual void onDraw(double now, float dt) = 0;
   virtual void onReset() = 0;
-  virtual std::string getName() = 0;
+
+  const std::string& getName() const {return _name;}
 
 protected:
-  Application* _app;
+  App* _owner;
+  std::string _name;
 };
 
-class Engine;
-
-class Application
+// Virtual base class for applications. Derive from this class and setup some
+// states to create a game.
+class App
 {
 public:
-  static constexpr Input::KeyCode pauseKey {Input::KEY_p};
+  App() = default;
+  virtual ~App() = default;
 
-public:
-  Application() = default;
-  virtual ~Application() = default;
-  
-  virtual bool initialize(Engine* engine, int32_t windowWidth, int32_t windowHeight);
+  // Invoked by the engine on boot. For use by derived classes to instantiate and
+  // add all their app states, as well as setting their initial state.
+  virtual bool onInit() = 0;
 
+  void onUpdate(double now, float dt)
+  {
+    _active->onUpdate(now, dt);
+  }
+
+  void onDraw(double now, float dt)
+  {
+    _active->onDraw(now, dt);
+  }
+
+  // For use by app states to switch between other states (game state, menu states etc).
+  void switchState(const std::string& name)
+  {
+    _active = _states[name];
+    _active->onReset();
+  }
+
+  // The app must provide this data to the engine so the window title can be
+  // composed.
   virtual std::string getName() const = 0;
-  virtual int32_t getVersionMajor() const = 0;
-  virtual int32_t getVersionMinor() const = 0;
+  virtual int getVersionMajor() const = 0;
+  virtual int getVersionMinor() const = 0;
 
-  void onWindowResize(int32_t windowWidth, int32_t windowHeight);
-  virtual void onUpdate(double now, float dt);
-  virtual void onDraw(double now, float dt);
-
-  void switchState(const std::string& name);
-
-  bool isWindowTooSmall() const {return _isWindowTooSmall;}
-
-protected:
+  // The app must provide this info to the engine so the gfx layers can be created to
+  // the size required by the app.
   virtual Vector2i getWorldSize() const = 0;
 
-  void addState(std::unique_ptr<ApplicationState>&& state);
-
-private:
-  void drawWindowTooSmall();
-
-private:
-  std::unordered_map<std::string, std::unique_ptr<ApplicationState>> _states;
-  std::unique_ptr<ApplicationState>* _activeState;
-  iRect _viewport;
-  Engine* _engine;
-  bool _isWindowTooSmall;
+protected:
+  std::unordered_map<std::string, std::shared_ptr<AppState>> _states;
+  std::shared_ptr<AppState> _active;
 };
 
+} // namespace pxr
+
+#endif
