0a3004926c3ebf47767a799d5a1fa5ac38b00077
ianmurfinxyz <ianmurfin@protonmail.com>
2022-03-12 15:13:41 +0000

    setup basic conan workflow

---
+++ conanfile.py
@@ -0,0 +1,50 @@
+from conans import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake
+
+class PixiRetroConan(ConanFile):
+	name = "PixiRetro"
+	version = "0.9.0"
+	
+	# optional metadata
+	license = "MIT"
+	author = "Ian Murfin - github.com/ianmurfinxyz - ianmurfin@protonmail.com"
+	url = "<Package recipe repository url here, for issues about the package>"
+	description = "A small framework for making 2D arcade games."
+	topics = ("Game Framework", "2D Games", "Arcade Games")
+	
+	# Binary configuration
+	settings = "os", "compiler", "build_type", "arch"
+	options = {"shared": [True, False], "fPIC": [True, False]}
+	default_options = {"shared": False, "fPIC": True}
+	
+	scm = {
+		"type": "git",
+		"subfolder": "Source",
+		"url": "auto",
+		"revision": "auto"
+	}
+	
+	def config_options(self):
+		if self.settings.os == "Windows":
+			del self.options.fPIC
+	
+	def generate(self):
+		tc = CMakeToolchain(self)
+		tc.generate()
+		
+	def requirements(self):
+		self.requires("tinyxml2/9.0.0")
+		self.requires("sdl/2.0.20")
+		self.requires("sdl_image/2.0.5")
+	
+	def build(self):
+		cmake = CMake(self)
+		cmake.configure()
+		cmake.build()
+	
+	def package(self):
+		cmake = CMake(self)
+		cmake.install()
+	
+	def package_info(self):
+		self.cpp_info.libs = ["PixiRetro"]
